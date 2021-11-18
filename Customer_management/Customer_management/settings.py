b7d5158d6597824b6bd4f93973d37c76b5375d43
Nihanglama <nihanglama@gmail.com>
2021-11-18 20:19:36 +0545

    heroku setup

--- Customer_management/Customer_management/settings.py
+++ Customer_management/Customer_management/settings.py
@@ -1,3 +1,5 @@
+import dj_database_url
+import django_heroku
 from pathlib import Path
 import os
 
@@ -12,9 +14,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-oip%(2xzszjs_$!g*d_j9ez2tnlqz)z1*sz7lkq7%w1!^*o@4q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nihang-customer-manage.herokuapp.com']
 
 
 # Application definition
@@ -39,6 +41,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Customer_management.urls'
@@ -112,6 +115,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_URL= '/images/'
@@ -136,3 +140,5 @@ EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_HOST_USER='djangotestmail1122@gmail.com'
 EMAIL_HOST_PASSWORD='djangoprojecttestmail'
+
+django_heroku.settings(locals())
